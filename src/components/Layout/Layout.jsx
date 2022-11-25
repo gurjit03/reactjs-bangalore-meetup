ca3067742f452f2fee0e217aa3d6e27ad2a0ef5e
gurjit03 <gurjitmehta@gmail.com>
2022-11-26 00:03:39 +0530

    chore: adds back-arrow

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,33 @@
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { getTitle } from "./utils";
+import "./style.css";
 
 const Layout = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const title = getTitle(location.pathname);
+  const isFriendsRoute = location.pathname.includes("friend");
   return (
     <div>
-      <h3 className="title">{title}</h3>
+      <div className="container">
+        {isFriendsRoute && (
+          <motion.img
+            src="/back-arrow.svg"
+            className="back-arrow"
+            whileHover={{
+              scale: 1.2,
+              cursor: "pointer",
+              transition: {
+                duration: 0.3,
+              },
+            }}
+            onClick={() => navigate("/")}
+          />
+        )}
+        <h3 className="title">{title}</h3>
+      </div>
       <AnimatePresence>{<Outlet />}</AnimatePresence>
     </div>
   );
